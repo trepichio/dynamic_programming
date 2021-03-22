6c4827621b28162245b26891b1556605ebdc49c0
trepichio <trepichio@gmail.com>
2021-03-22 17:33:45 -0300

    solving Fibonacci using tabulation pattern

---
+++ tabulation/fib.js
@@ -0,0 +1,17 @@
+const fib = (n) => {
+  const table = Array(n + 1).fill(0)
+  table[1] = 1
+
+  for (let i = 0; i <= n; i++) {
+    table[i + 1] += table[i]
+    table[i + 2] += table[i]
+  }
+
+  return table[n]
+}
+
+console.log(fib(6)); //8
+console.log(fib(7)); //13
+console.log(fib(8)); //21
+console.log(fib(50)); //12586269025
+console.log(fib(500)); //1.394232245616977e+104
