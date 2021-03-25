6587eb138fb07e9739330447527869bed58e9a51
trepichio <trepichio@gmail.com>
2021-03-25 00:14:34 -0300

    solving bestSum using tabulation pattern

---
+++ tabulation/bestSum.js
@@ -0,0 +1,26 @@
+const bestSum = (targetSum, numbers) => {
+  const table = Array(targetSum + 1).fill(null)
+
+  table[0] = []
+
+  for (let i = 0; i <= targetSum; i++) {
+    if (table[i] !== null) {
+      for (const num of numbers) {
+
+        const combination = [...table[i], num]
+
+        if (!table[i + num] || table[i + num].length > combination.length)
+          table[i + num] = combination
+      }
+    }
+
+  }
+
+  return table[targetSum]
+}
+
+
+console.log(bestSum(7, [5, 3, 4, 7])); // [7]
+console.log(bestSum(8, [2, 3, 5])); // [3, 5]
+console.log(bestSum(8, [1, 4, 5])); // [4, 4]
+console.log(bestSum(100, [1, 2, 5, 25])); // [25, 25, 25, 25]
